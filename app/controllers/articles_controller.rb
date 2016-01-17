2638d093ec73b0416ebb12feff9f4bec118144da
Brian <brian.diaz@stonybrook.edu>
2016-01-17 18:18:45 +0000

    Fix create, add falsh and error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,14 +2,32 @@ class ArticlesController <ApplicationController
     def new 
         @article=Article.new
     end
-    
-    def create
-        @article=Article.create(article_params)
-        redirect_to articles_show(@article)
+    #def index
+       # @articles=Article.all
+   # end
+    def show
+          @article=Article.find(params[:id])
     end
+    def create
+        @article=Article.new(article_params)
+        #redirect_to articles_path(@article)
+        if @article.save
+            flash[:notice]="Article was successfully saved"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+            
+    end
+   
+    
+   
     
     private 
       def article_params
-        params.require(:article).permit(:title, :description)
+            params.require(:article).permit(:title, :description)
       end
+    
+      
+      
 end
