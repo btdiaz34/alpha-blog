e4c124a509f16212edfe0d78fb08562beaca95d9
Brian <brian.diaz@stonybrook.edu>
2016-01-18 04:20:28 +0000

    Extract reduancy and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,13 @@
 class ArticlesController <ApplicationController
+   before_action :set_article, only: [:edit,:update,:show,:destroy]
    
     
     def new 
         @article=Article.new
     end
     def destroy
-        @article=Article.find(params[:id])
+        
+        
         @article.destroy
        
         
@@ -14,10 +16,11 @@ class ArticlesController <ApplicationController
          redirect_to articles_path
     end
     def edit
-        @article=Article.find(params[:id])
+       
     end
     def update
-        @article=Article.find(params[:id])
+        
+        
         if @article.update(article_params)
             flash[:notice]="Article was successfully updated"
             redirect_to article_path(@article)
@@ -29,7 +32,8 @@ class ArticlesController <ApplicationController
         @articles=Article.all
     end
     def show
-          @article=Article.find(params[:id])
+        
+          
     end
     def create
         @article=Article.new(article_params)
@@ -49,6 +53,10 @@ class ArticlesController <ApplicationController
     private 
       def article_params
             params.require(:article).permit(:title, :description)
+      end
+      
+      def set_article
+          @article=Article.find(params[:id])
       end
     
       
